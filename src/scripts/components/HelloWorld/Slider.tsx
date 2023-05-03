63bd4aa8e7a1cc853c46a0f3d05bc22e68016a1f
Luis C. Rojas <luisrojas@blacksip.com>
2023-05-03 17:15:23 -0500

    feat(slider): :beers: añadir componente `Slider` de ejemplo
    Se añade un componente `Slider` de ejemplo que renderiza imagenes
    
    Ejemplo `Slider`

---
+++ src/scripts/components/HelloWorld/Slider.tsx
@@ -0,0 +1,38 @@
+import { h } from 'preact'
+import { useState } from 'preact/hooks'
+
+interface SliderProps {
+  images: string[]
+}
+
+const Slider = ({ images }: SliderProps) => {
+  const totalImages = images?.length
+
+  const [visibleImage, setVisibleImage] = useState(0)
+
+  const handleNextImage = () => {
+    setVisibleImage(visibleImage === totalImages - 1 ? 0 : visibleImage + 1)
+  }
+
+  const handlePrevImage = () => {
+    setVisibleImage(visibleImage === 0 ? totalImages - 1 : visibleImage - 1)
+  }
+
+  return (
+    <section className="sliderContainer">
+      <button onClick={handlePrevImage} className="sliderArrows sliderArrowLeft">
+        &#8249;
+      </button>
+      {images.map((image, index) => (
+        <div className="sliderContent">
+          {visibleImage === index && <img className="sliderImage" key={index} src={image} alt={index.toString()} />}
+        </div>
+      ))}
+      <button onClick={handleNextImage} className="sliderArrows sliderArrowRight">
+        &#8250;
+      </button>
+    </section>
+  )
+}
+
+export default Slider
--- src/scripts/components/HelloWorld/index.tsx
+++ src/scripts/components/HelloWorld/index.tsx
@@ -1,8 +1,19 @@
 import { h, FunctionComponent } from 'preact'
 import useOrderForm from '../../../utils/useOrderForm'
+import Slider from './Slider'
 
 const HelloWorld: FunctionComponent = () => {
   const { orderForm, clientProfileData } = useOrderForm()
+  const images = [
+    'https://picsum.photos/id/237/1920/500',
+    'https://picsum.photos/id/238/1920/500',
+    'https://picsum.photos/id/239/1920/500',
+    'https://picsum.photos/id/240/1920/500',
+    'https://picsum.photos/id/241/1920/500',
+    'https://picsum.photos/id/242/1920/500',
+    'https://picsum.photos/id/243/1920/500',
+  ]
+
   return (
     <div>
       <h1 className="title-preact-component">
@@ -12,6 +23,7 @@ const HelloWorld: FunctionComponent = () => {
       <p>
         Total products in cart: <b>{orderForm?.items?.length}</b>
       </p>
+      <Slider images={images} />
     </div>
   )
 }
